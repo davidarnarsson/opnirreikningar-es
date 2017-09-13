6ed84809e59c9c5fccb8048c74488d537e3175b3
Davíð Arnarsson <david.arnarsson@advania.is>
2017-09-13 10:00:27 +0000

    working

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 const makeClient = require("./elastic");
-//const { Subject, Processor } = require("./processor");
 const moment = require("moment");
 const { maxTimePeriod, getInvoices } = require("./reikningar");
 const { Observable } = require("rxjs/Observable");
 const { BehaviorSubject } = require("rxjs/BehaviorSubject");
 
 require("rxjs/add/observable/interval");
-require("rxjs/add/observable/of");
 require("rxjs/add/observable/from");
-require("rxjs/add/observable/fromPromise");
 require("rxjs/add/operator/timeout");
 require("rxjs/add/operator/zip");
+require("rxjs/add/operator/toPromise");
 require("rxjs/add/operator/map");
+require("rxjs/add/operator/scan");
 require("rxjs/add/operator/concatAll");
-require("rxjs/add/operator/combineAll");
 require("rxjs/add/operator/concatMap");
 require("rxjs/add/operator/do");
 
@@ -54,13 +52,7 @@ async function run() {
     length
   });
 
-  const indexItem = item => {
-    console.log(`Indexing invoice ${item.invoice_id}`);
-    return client.index(argv.in || "reikningar", argv.it || "item", item.invoice_id, item);
-  };
-
   const subject = new BehaviorSubject([]);
-  subject.next(makeBatchRequest(0));
 
   Observable.interval(argv.i || 1000)
     .timeout(argv.o || 3000)
@@ -70,49 +62,25 @@ async function run() {
       return getInvoices(fromDate, toDate, start, length);
     })
     .concatAll()
-    .concatMap(arr => Observable.from(arr))
-    .concatMap(indexItem)
-    .combineAll()
-    .do(arr => {
-      console.log(`Indexed ${batchSize * current} - ${batchSize * current + batchSize} ${arr}`);
-      if (arr.length >= batchSize) {
-        subject.next(makeBatchRequest(++current));
+    .concatMap(arr => Observable.from(arr)
+      .concatMap(item => {
+        console.log(`Indexing invoice ${item.invoice_id}`);
+        return client.index(argv.in || "reikningar", argv.it || "item", item.invoice_id, item);
+      })
+      .scan(x => x + 1, 0)
+      .toPromise())
+    .do(count => {
+      console.log(`Indexed ${batchSize * current} - ${batchSize * current + batchSize}`);
+      if (count >= batchSize) {
+        subject.next(makeBatchRequest((++current) * batchSize));
       } else {
         subject.complete();
       }
     })
-    .subscribe(v => console.log(v), e => console.error(e), () => console.log("Complete?"));
+    .subscribe(null, e => console.error(e), () => console.log("Complete!"));
 
-  //   const subject = new Subject()
-  //     .processSingleCallback(({ fromDate, toDate, start, length }) => {
-  //       console.log(
-  //         `Request: Fetching ${start * batchSize}-${start * batchSize + length}`
-  //       );
-  //       return getInvoices(fromDate, toDate, start, length);
-  //     })
-  //     .handleResultCallback(async rs => {
-  //       const data = await rs;
-
-  //       console.log(`Response: Got ${data.length} items.`);
-
-  //       // weird off-by-one error going on here, too lazy to figure out
-  //       // whether its my code or the website's code.
-  //       if (data.length === batchSize + 1) {
-  //         subject.next(makeBatchRequest(current++));
-  //       } else {
-  //         subject.complete();
-  //       }
-  //     });
-
-  //   subject.next(makeBatchRequest(current++));
-
-  //   const processor = new Processor()
-  //     .timeout(argv.o || 3000)
-  //     .interval(argv.i || 1000)
-  //     .process(subject)
-  //     .error(err => console.error(err))
-  //     .complete(() => console.log("Done?"));
-  // }
+    // choo choo!
+    subject.next(makeBatchRequest(0));
 }
 
 try {
